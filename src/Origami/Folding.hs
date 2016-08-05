08c985ce81eaf08526e4beb2de836a2e23ceecb1
Scott Murphy <scottmurphy09@gmail.com>
2016-08-05 01:38:00 -0500

    More util functions

--- src/Origami/Folding.hs
+++ src/Origami/Folding.hs
@@ -48,21 +48,33 @@ data Segment = Segment {  v0 :: Vertex
 
 
 -- Needs check
+-- Create connected line segments
+cycleNeighbors :: Vector Vertex -> Vector Origami.Folding.Segment
 cycleNeighbors vs = cycleIfLong  
           where
              cycleIfLong
-               | Vector.length vs >= 2 = Vector.fromList . Vector.foldl' buildSegments buildOneSegment (Vector.drop 2 vs)
+               | Vector.length vs >= 2 = Vector.fromList . Vector.foldl' buildSegments buildOneSegment $ (Vector.drop 2 vs)
                | otherwise = error "length should be at least 2 for a segment"
-             buildOneSegment = Segment (vs!0) (vs!1)
-             buildSegments v (seg:segs) = (Segment (v0 seg) v ): seg : segs
+             buildOneSegment = [Segment (vs!0) (vs!1)]
+
+             
+             buildSegments (seg:segs) v = (Segment (v0 seg) v ): seg : segs
 
 
+-- Make sure the point is in the polygon
+testPointInside = (pointInside (V2 1.5 0.5) (Vector.fromList [(V2 0 0), (V2 1 0), (V2 1 1), (V2 0 1)]) == False) &&
+                 (pointInside (V2 0.5 0.5) (Vector.fromList [(V2 0 0), (V2 1 0), (V2 1 1), (V2 0 1)]) == True) 
 
 
-pointInside (V2 x y) vs = (Vector.length intersectPairs) `mod` 2 == 1
+pointInside :: V2 Fraction -> Vector Vertex -> Bool
+pointInside (V2 x y) vs = (Vector.length intersectSegments) `mod` 2 == 1
   where
-     intersectPairs = Vector.filter (\p -> positiveXAxis p && aboveBelow p) allPairs
-     
+     intersectSegments = Vector.filter (\p -> positiveXAxis p && aboveBelow p) allSegments
+     allSegments = cycleNeighbors vs
+     positiveXAxis (Segment (V2 x0 _ ) (V2 x1 _))  =  (x0 > x) || (x1 > x )
+
+     aboveBelow (Segment (V2 _ y0) (V2 _ y1))   = ((y0 > y) && (y1 < y)) ||
+                                                     ((y0 < y) && (y1 > y))
 exteriorVertex (V2 x y) = _
 
 hullFacets paper =   toList . toVertex. facets $ paper
@@ -106,12 +118,12 @@ Crease must be orthogonal to the direction of the fold
 
 
 
-foldPaper :: SourcePaper -> VertexToMove -> VertexToTarget -> SinkPaper
-foldPaper source vM vT = _
-     where
-       foldDirection    = makeVector vM vT 
-       foldCrease       = orthVector foldDirection
-       validNewPaper    = findCrease source foldCrease
+-- foldPaper :: SourcePaper -> VertexToMove -> VertexToTarget -> SinkPaper
+-- foldPaper source vM vT = _
+--      where
+--        foldDirection    = makeVector vM vT 
+--        foldCrease       = orthVector foldDirection
+--        validNewPaper    = findCrease source foldCrease
 
 
 
