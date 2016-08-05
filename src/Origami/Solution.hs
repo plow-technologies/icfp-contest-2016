e2734f1643f5f944ed7646bd9ffcec35051115ca
Edward Amsden <edwardamsden@gmail.com>
2016-08-05 01:48:27 -0400

    Solution type and pretty-printer

--- src/Origami/Solution.hs
+++ src/Origami/Solution.hs
@@ -1,4 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Origami.Solution where
 
-baz :: Int
-baz = 16
+import           Origami.Silhouette
+import           Data.Text (Text)
+import qualified Data.Text as Text
+
+data Solution
+  = Solution
+  { _solutionPositions :: [Position]
+  , _solutionPolygons :: [[Int]]
+  }
+
+data Position
+  = Position
+  { _positionSource :: !Coordinate
+  , _positionDestination :: !Coordinate
+  }
+
+prettySources :: [Position] -> Text
+prettySources positions = 
+    Text.unlines
+  $ (Text.pack $ show $ length positions)
+  : map (prettyCoordinate . _positionSource) positions
+
+prettyPolygons :: [[Int]] -> Text
+prettyPolygons polygons =
+    Text.unlines
+  $ (Text.pack $ show $ length polygons)
+  : map (Text.intercalate " " . map (Text.pack . show)) polygons
+
+prettyDestinations :: [Position] -> Text
+prettyDestinations positions =
+    Text.unlines
+  $ map (prettyCoordinate . _positionDestination) positions
+
+prettySolution :: Solution -> Text
+prettySolution (Solution positions polygons) =
+  Text.unlines
+  [ prettySources positions
+  , prettyPolygons polygons
+  , prettyDestinations positions
+  ]
